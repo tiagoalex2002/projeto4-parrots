7347cb06bf05737dabf03edb713c8b3c8d9c9c64
tiagoalex2002 <tiagoalex0202@gmail.com>
2023-01-16 09:55:14 -0300

    Java não finalizado : rotacao das cartas funciona

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ let j = 0;
 while (j < q) {
     const elemento = document.querySelector(".container-de-cartas");
     elemento.innerHTML += `
-    <div data-test="card" onclick = "rotacionar(this)"class="carta">
+    <div data-test="card" onclick = "rotacionar(this)" class="carta">
         <div class="front-face face">
             <img data-test="face-down-image" class="back" src="imagens/back.png" alt="back">
         </div>
@@ -60,7 +60,7 @@ function rotacionar(card) {
     const cartasViradas = [];
     const cartacerta = card.classList.contains('correto');
     const rotacionado = card.classList.contains('virado');
-    if (cartacerta == false && rotacionado == true && timeout == true) {
+    if (cartacerta == false && rotacionado == false && timeout == true) {
         card.classList.add('virado');
         card.querySelector(".back-face").classList.toggle("girar-back-face");
         card.querySelector(".front-face").classList.toggle("girar-front-face");
@@ -77,13 +77,11 @@ function rotacionar(card) {
             imagem1 = carta1.querySelector('.back-face img').src;
         }
 
-        if (cartasViradas.length == 2) {
+        else if (cartasViradas.length == 2) {
             carta2 = card;
             imagem2 = carta2.querySelector('.back-face img').src
         }
 
-        const possui1 = carta1.classList.contains('virado');
-        const possui2 = carta2.classList.contains('virado');
         if (cartasViradas.length == 2 && imagem1 != imagem2) {
             cartasViradas.length = 0;
             carta1.classList.remove('virado');
